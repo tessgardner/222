1f0fed928541c7b885a6f1d0afc5023c4beb676d
tess 🩷🩷🩷 <166083519+tessgardner@users.noreply.github.com>
2025-01-16 14:43:43 -0600

    Working on question1

--- 222/cprograms/Lab3/question1.c
+++ 222/cprograms/Lab3/question1.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char *argv[]){
+    // ^ a standard signature for taking in command line arguments
+    // argc represents the number of arguments coming in. 
+    // argv a pointer to where the arguments are stored in memory
+    
+    // printing the inputs
+    printf("argc: %d\n", argc);
+    
+    for (int i=0; i < argc; i++){
+        printf("argv[%d]: %s\n", i, argv[i]);
+    }
+
+    // argv ends with a null pointer
+    printf("argv[%d]: %s\n", argc, argv[argc]);
 
     return 0;
 }

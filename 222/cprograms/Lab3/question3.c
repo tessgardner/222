504f62b64b55932592b210ee47471ab6c434fb71
Tess Gardner <tng018@email.latech.edu>
2025-01-20 06:29:10 +0000

    Going to sleep

--- 222/cprograms/Lab3/question3.c
+++ 222/cprograms/Lab3/question3.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
 int main() {
+    int intInput = 0; 
+
+        printf("Enter a negative value at any point to quit.\n");
+
+      
+
+        //while (intInput >= 0) {
+            printf("Enter an integer: ");
+            scanf("%d", &intInput);
+            // work will go here
+            for (int i = 2; i <= 100; i*i) {
+                printf("%d ", i);
+            }
+            
+        //}
+
+        printf("Bye.");
 
     return 0;
 }

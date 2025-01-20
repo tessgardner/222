dcab7a5cea35de72cf8746d19b03ec9fc5f25f6b
Tess Gardner <tng018@email.latech.edu>
2025-01-20 06:14:58 +0000

    Adding in a few comments

--- 222/cprograms/Lab3/question1.c
+++ 222/cprograms/Lab3/question1.c
@@ -2,27 +2,27 @@
 
 int main(int argc, char *argv[]){
     
-
+    // ensuring argument sizes are correct
     if (argc < 3) {
         printf("Must provide at least 2 arguments.");
     } 
     else if (argc > 7) {
         printf("Must provide at most 6 arguments.\n");
     }
-    else {
-            for (int i=1; i < argc; i++){
-        if (i % 2 == 0) {
-            /*found an article on narkive(newsgroup archive) for how 
-            to access individual char or a string in an array */
-            printf("%c ", argv[i][0]);
-        }
-        else {
-            printf("%c ", argv[i][1]);
-        }
-    }
 
-    printf("\n");
+    else {
+        for (int i=1; i < argc; i++){
+            // returning either 1st or 2nd char of each word!
+            if (i % 2 == 0) {
+                /*found an article on narkive(newsgroup archive) for how 
+                to access individual char or a string in an array */
+                printf("%c ", argv[i][0]);
+            }
+            else {
+                printf("%c ", argv[i][1]);
+            }
+        }
+        printf("\n");
     }
-    
     return 0;
 }
